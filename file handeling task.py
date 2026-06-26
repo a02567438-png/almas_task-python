7827f0f5001475889f3fbee6303eb244ca6294c0
a02567438-png <a02567438@gmail.com>
2026-06-26 08:03:31 +0530

    Add files via upload

---
+++ file handeling task.py
@@ -0,0 +1,40 @@
+#task
+##a=input("enter std name")
+##b=(input("enter std mark"))
+##c=(input("enter std id"))
+##f=open(r"C:\Users\a0256\OneDrive\Desktop\college_details.txt","a")
+##f.write(a+"\n"+b+"\n"+c)
+##f.close()
+
+#login 
+##f=open(r"C:\Users\a0256\OneDrive\Desktop\pass_details.txt","w")
+##a=input("enter your name")
+##b=input("enter pass")
+##f.write(a+"\n"+b)
+##f.close()
+
+#attendance
+##f=open(r"C:\Users\a0256\OneDrive\Desktop\new folder.txt","a")
+##a=input("enter the name")
+##b=input("enter date")
+##c=input("entering their attendance")
+##f.write(a+"\n"+b+"\n"+c)
+##f.close()
+
+#bank
+##a=input("enter whether it is a deposit or withdrawl")
+##b=input("enter the amount")
+##f=open(r"C:\Users\a0256\OneDrive\Desktop\bank_trans","a")
+##if a=="deposit":
+##    f.write(a+"\n"+b)
+##    print("amount has been deposited")
+##else:
+##    f.write(a+"\n"+b)
+##    print("withdrawl amount")
+##f.close()
+
+
+
+
+
+
